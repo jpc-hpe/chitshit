bf7b2ed842c7994fb7e5b76011f68b8f5d6c538b
Juan Postlbauer <juan.postlbauer@hpe.com>
2025-07-24 18:22:28 +0200

    Debug callback information

--- lua/cheetah/commands/init.lua
+++ lua/cheetah/commands/init.lua
@@ -19,7 +19,24 @@ function get_keymaps()
   for _, map in ipairs(keymaps) do
     print("Keymap:")
     for key, value in pairs(map) do
-      print(string.format("  %s: %s", key, value))
+      if key == "callback" and type(value) == "function" then
+        -- Try to get more info about the function
+        local info = debug.getinfo(value, "S")
+        if info then
+          print(string.format("  %s: function defined in %s at line %s", 
+            key, info.short_src or "unknown source", info.linedefined or "unknown"))
+          
+          -- Try to get the function name
+          local funcName = debug.getinfo(value, "n").name
+          if funcName then
+            print(string.format("  function name: %s", funcName))
+          end
+        else
+          print(string.format("  %s: function: %s", key, tostring(value)))
+        end
+      else
+        print(string.format("  %s: %s", key, tostring(value)))
+      end
     end
     print("-------------------")
   end
