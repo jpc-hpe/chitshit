c438f2295cb055fd3634fb4bdccf7ef5039a421b
Juan Postlbauer <juan.postlbauer@hpe.com>
2025-07-24 18:05:08 +0200

    Check plain print behaviour

--- lua/cheetah/commands/init.lua
+++ lua/cheetah/commands/init.lua
@@ -4,12 +4,21 @@ local M = {}
 
 -- Example command implementation
 function M.hello_command()
+  print("Just using print")
   utils.print_message("Hello cheetah!") -- same as info level
   -- utils.print_message("warning from cheetah plugin command!", "warn")
   -- utils.print_message("error from cheetah plugin command!", "error")
   -- utils.print_message("info from cheetah plugin command!", "info")
+  -- get_keymaps()
 
   
+end
+
+function get_keymaps()
+  local keymaps = vim.api.nvim_get_keymap() -- Get keymaps for all modes
+  for _, map in ipairs(keymaps) do
+    print(string.format("Key: %s, Command: %s", map.lhs, map.rhs))
+  end
 end
 
 -- Add more commands as needed
